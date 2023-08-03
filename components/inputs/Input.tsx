49f6576cb279e8f4d53e28d79584cdab21376e08
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-08-03 17:48:49 +0800

    Update Input.tsx

--- components/inputs/Input.tsx
+++ components/inputs/Input.tsx
@@ -47,7 +47,8 @@ const Input: React.FC<InputProps> = ({
             w-full 
             rounded-md 
             border-0 
-            py-1.5 
+            py-1.5
+            px-2
             text-gray-900 
             shadow-sm 
             ring-1 
