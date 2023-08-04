0b5d175e4d96fdfdbb23bb798a1997835076178a
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-08-04 11:33:34 +0800

    Update route.ts

--- app/api/messages/route.ts
+++ app/api/messages/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 import getCurrentUser from "@/actions/getCurrentUser";
-// import { pusherServer } from "@/libs/pusher";
+import { pusherServer } from "@/libs/pusher";
 import prisma from "@/libs/prismadb";
 
 export async function POST(request: Request) {
@@ -58,19 +58,19 @@ export async function POST(request: Request) {
 			},
 		});
 
-		// await pusherServer.trigger(conversationId, "messages:new", newMessage);
+		await pusherServer.trigger(conversationId, "messages:new", newMessage);
 
-		// const lastMessage =
-		// 	updatedConversation.messages[
-		// 		updatedConversation.messages.length - 1
-		// 	];
+		const lastMessage =
+			updatedConversation.messages[
+				updatedConversation.messages.length - 1
+			];
 
-		// updatedConversation.users.map((user) => {
-		// 	pusherServer.trigger(user.email!, "conversation:update", {
-		// 		id: conversationId,
-		// 		messages: [lastMessage],
-		// 	});
-		// });
+		updatedConversation.users.map((user) => {
+			pusherServer.trigger(user.email!, "conversation:update", {
+				id: conversationId,
+				messages: [lastMessage],
+			});
+		});
 
 		return NextResponse.json(newMessage);
 	} catch (error) {
