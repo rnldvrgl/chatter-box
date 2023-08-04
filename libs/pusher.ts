4faad43fd4fe798bf94c2b5306c7b0a0d7b56a41
Ronald Vergel Dela Cruz <DelaCruz.RonaldVergel@gmail.com>
2023-08-04 13:46:33 +0800

    update

--- libs/pusher.ts
+++ libs/pusher.ts
@@ -5,13 +5,13 @@ export const pusherServer = new PusherServer({
 	appId: process.env.PUSHER_APP_ID!,
 	key: process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
 	secret: process.env.PUSHER_SECRET!,
-	cluster: process.env.PUSHER_CLUSTER!,
+	cluster: "ap1",
 	useTLS: true,
 });
 
 export const pusherClient = new PusherClient(
 	process.env.NEXT_PUBLIC_PUSHER_APP_KEY!,
 	{
-		cluster: process.env.NEXT_PUBLIC_PUSHER_CLUSTER!,
+		cluster: "ap1",
 	}
 );
